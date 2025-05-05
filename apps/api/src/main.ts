72163af6747ef166cc13277d572b6ac202d6b0a5
Remidee <remi.aluko@expatswap.com>
2025-05-05 10:23:08 +0100

    fix: add deployed url to cor senabled origin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:3001'],
+    origin: [
+      process.env.FRONTEND_URL || 'http://localhost:3001',
+      'https://url-shortener-dxks.onrender.com',
+    ],
     methods: ['GET', 'POST', 'DELETE'],
     credentials: true,
   });
